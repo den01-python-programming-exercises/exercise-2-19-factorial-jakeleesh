bd61de9ac6abcb5e81d313cfcf56ab922cc72a5b
Jake Lee <jake.lee@fdmgroup.com>
2021-09-02 22:18:10 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    last = int(input("Give a number:"))
+    factorial = 1
+    for i in range(1, last + 1):
+        factorial = factorial * i
+    print("Factorial: " + str(factorial))
 
 if __name__ == '__main__':
     main()
